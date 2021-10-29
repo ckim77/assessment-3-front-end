9811abbee1e2b915881c08d09c62642f0f290325
Chris Kim <ckjr92@gmail.com>
2021-10-29 09:40:11 -0600

    finished step 2

--- about.js
+++ about.js
@@ -5,10 +5,19 @@ console.log("hello world");
 function handleSubmit(evt) {
 	evt.preventDefault();
 	
-	console.log('form submit');
+	alert('Your input was successful!');
 }
 
 
 let form = document.querySelector('form#contact');
 
 form.addEventListener('submit', handleSubmit);
+
+const pictureCompliment = event => {
+	event.preventDefault;
+	alert('Why are you so beautiful');
+}
+
+let ducko = document.getElementById("ducko");
+
+ducko.addEventListener('mouseover', pictureCompliment);

65b1226913cdcb00b678fd1a8db9e2c5d46fc632
Chris Kim <ckjr92@gmail.com>
2021-10-29 15:08:19 -0600

    added some more changes during review

--- neighborhood.js
+++ neighborhood.js
@@ -1,12 +1,16 @@
 let recommendBtn = document.getElementById("restaurant-button");
 let restArray = ['Pho Plus', 'Bombay House', 'J Dawgs', 'The Wash', 'In n Out']
+const restaurantInfo = document.querySelector(".restaurant-info");
 
 
-
-const recommendRestaurant = () => {
+const recommendRestaurant = (evt) => {
+    evt.preventDefault();
     let random = Math.floor(Math.random() * restArray.length);
-    alert((random, restArray[random]));
+    restaurantInfo.textContent = restArray[random];
+    // alert((random, restArray[random]));
 }
+
+
 
 recommendBtn.addEventListener('click',recommendRestaurant);
 
